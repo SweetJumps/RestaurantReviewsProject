8a8f832a03ff2abeaf0114df5935da7251eb0fd6
SweetJumps <50113914+SweetJumps@users.noreply.github.com>
2019-07-28 20:57:51 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -180,6 +180,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.tabIndex = "3";
   more.setAttribute('aria-label', `Details for ${restaurant.name}`);
   li.append(more)
 
@@ -205,34 +206,10 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 //IndexController.prototype._registerServiceWorker = function () {
   if (navigator.serviceWorker) {
 
-    var indexController = this;
-
     navigator.serviceWorker.register('/sw.js').then(function (reg) {
       if (!navigator.serviceWorker.controller) {
         return;
       }
 
-      if (reg.waiting) {
-        indexController._updateReady(reg.waiting);
-        return;
-      }
-
-      if (reg.installing) {
-        indexController._trackInstalling(reg.installing);
-        return;
-      }
-
-      reg.addEventListener('updatefound', function () {
-        indexController._trackInstalling(reg.installing);
-      });
     });
 }
-  // Ensure refresh is only called once.
-  // This works around a bug in "force update on reload".
-  //var refreshing;
-  //navigator.serviceWorker.addEventListener('controllerchange', function () {
-  //  if (refreshing) return;
-  //  window.location.reload();
-  //  refreshing = true;
-  //});
-//};
